da2d36f309c4da90dfe43d2517d3017c436e90fe
Marcus H <90532464+voyager1winterberry@users.noreply.github.com>
2024-02-22 17:56:49 -0800

    Update App.jsx

--- public/App.jsx
+++ public/App.jsx
@@ -1,3 +1,10 @@
+/*
+the transformation of JSX to JavaScript happened at runtime with past
+code. this is inefficient. here, the transformation is moved to the 
+build state in our development so that a ready-to-use version 
+can be deployed.
+*/
+
 const element = (
   <div title="Outer div">
     <h1>Hello World!</h1>
